337b22bbe0b94e9cea7edebc5f1ce5145299b8a1
Marco Guerri <gmarco.dev@gmail.com>
2014-05-01 15:51:35 +0200

    Working example which reads a bin file looking for a factor of the public key

--- main.cpp
+++ main.cpp
@@ -2,31 +2,52 @@
 #include <openssl/rsa.h>
 #include <openssl/pem.h>
 #include <openssl/bn.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+#define BIT_KEY 128
+#define SIZE_DUMP 334
+
 int main() {
-    FILE *fp;
+
+    FILE *fp_pk, *fp_bleed;
     RSA *pk = NULL;
-    char *q_hex;
     BN_CTX *temp;
-    BIGNUM *result;
-    int ret;
-    FILE *fp_out;
-
-
+    BIGNUM *pk_q_test, *reminder;
+    unsigned int cycle, num_read, i;
+    char *buff;
+    
     temp = BN_CTX_new();
-    result = BN_new();
-    fp = fopen("private_unencrypted.pem", "r");
-    pk = PEM_read_RSAPrivateKey(fp,NULL,NULL,NULL);
+    pk_q_test = BN_new();
+    reminder = BN_new();
+    //
+    // Extracting info from private key pem file
+    //
+    fp_pk = fopen("private_unencrypted.pem", "rb");
+    pk = PEM_read_RSAPrivateKey(fp_pk,NULL,NULL,NULL);
     if(pk == NULL) {
-        printf("Error while reading the private key");
-        return 1;
+       printf("Error while reading the private key");
+       return 1;
     }
-    ret = BN_mul(result,pk->q, pk->p, temp);
-    printf("RET IS %d\n", ret);
-    printf("p is: %s\n", BN_bn2hex(pk->p));    
-    printf("q is: %s\n", BN_bn2hex(pk->q));    
-    printf("result is %s\n", BN_bn2hex(result));
-    printf("N is %s\n", BN_bn2hex(pk->n));
-    fp_out = fopen("key_dumped", "w");
-    BN_print_fp(fp_out,  pk->q);
-    fclose(fp_out); 
-}
+
+    buff = (char *) malloc(SIZE_DUMP*sizeof(char));
+    fp_bleed = fopen("key_dumped_scrambled", "r");
+    cycle = 0;
+    num_read = fread((void*) buff, SIZE_DUMP, 1, fp_bleed);
+
+    for(i=0; i<SIZE_DUMP-BIT_KEY+1; i++) {
+        pk_q_test = BN_new();
+        BN_bin2bn((unsigned char*)buff+cycle ,BIT_KEY, pk_q_test);
+        BN_mod(reminder, pk->n, pk_q_test, temp);
+        if(BN_is_zero(reminder)) {
+            printf("I FOUND THE KEYYYYYY!\n");
+            printf("%s\n",BN_bn2hex(pk_q_test));
+            return 0;
+        }
+        cycle++;
+    }
+    return 0;
+   
+} 
